a4732ccb613825c92aad7fb554099cdc61800efa
unsemu <younes.moussaif@visium.ch>
2023-02-15 10:11:38 +0000

    Add rotation and crop

--- dvc_workshop/pipeline/preprocess/preprocessing.py
+++ dvc_workshop/pipeline/preprocess/preprocessing.py
@@ -28,6 +28,9 @@ def main() -> None:
     # Resize the images
     images = standardize(images)
 
+    # Rotate and crop the images
+    images = rotate_and_crop_images(images, angle_interval=(-5, 5))
+
     # Save the images
     save_images(images, target_directory=TARGET_DIRECTORY)
 
@@ -58,6 +61,49 @@ def standardize(images: dict, tolerance: float = 1e-5) -> dict:
         logging.warning("Standard deviation too small, we will only substract the mean.")
         for image_path, image in tqdm(images.items()):
             images[image_path] = image - mean
+
+    return images
+
+
+def rotate_and_crop_images(images: dict, angle_interval: tuple[float, float]) -> dict:
+    """Rotate and center crop an image."""
+
+    # pylint: disable=no-member
+    def _rotate_image(image: np.array, angle: float) -> np.array:
+        """Rotate an image."""
+        image_center = tuple(np.array(image.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        return result
+
+    def _crop_image(image: np.array, width: int, height: int) -> np.array:
+        """Center crop an image to the desired dimension."""
+        center = image.shape
+        x = center[1] / 2 - width / 2
+        y = center[0] / 2 - height / 2
+
+        cropped_image = image[int(y) : int(y + height), int(x) : int(x + width)]
+        return cropped_image
+
+    for image_path, image in tqdm(images.items()):
+        image_shape = image.shape
+        (original_width, original_height) = image_shape[0], image_shape[1]
+
+        assert (original_width % 2 == 0) and (original_height % 2 == 0), "Image dimensions are not even!"
+
+        # rotate with random angle in angle_interval
+        random_angle = float(np.random.uniform(low=angle_interval[0], high=angle_interval[1], size=1))
+        rotated_image = _rotate_image(image, angle=random_angle)
+
+        # center crop
+        cropped_image = _crop_image(rotated_image, original_width, original_height)
+        cropped_image_shape = cropped_image.shape
+
+        assert (original_width == cropped_image_shape[0]) and (
+            original_height == cropped_image_shape[1]
+        ), "Image dimension changes after resize and crop!"
+
+        images[image_path] = cropped_image
 
     return images
 
